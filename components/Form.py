866d4a9417e41da90958db39f9ff66d0a8a464f2
razor <general@enkit.ru>
2024-03-09 23:28:18 +0700

    Испульзовал компонентный подход к решению задачи. Появились переиспользуемые компоненты, которые создают variable и самостоятельно справляются с валидацией. Взял ранее готовый код из лабораторной работы с табуляцией. Адаптировал под текущие задачи.

--- components/Form.py
+++ components/Form.py
@@ -1,35 +1,61 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
-from tkinter import Button
-from tkinter import Entry
-from tkinter import Frame
-from tkinter import Label
+from utils.validate import validate_float
+from .Entry import Entry
+from .CheckBox import CheckBox
+from .ComboBox import ComboBox
 
 
 class Form:
-    def __init__(self):
+    def __init__(self, submit_callback):
+        self.submit_callback = submit_callback
         window = tk.Tk()
-        window.title("Табулирование функции tkinter")
 
-        frame = Frame(window, padx=10, pady=10)
+        window.title("Табулирование функции tkinter")
+        window.geometry("800x600")
+
+        label = tk.Label(window, text = "Функция y = f(x)")
+        label.pack()
+
+        frame = tk.Frame(window, padx=10, pady=10)
         frame.pack(expand=True)
 
-        start_label = Label(frame, text="Введите начальное значение")
-        start_label.grid(row=3, column=1)
+        self.start = Entry(frame, 'Введите начальное значение', 3)
+        self.end = Entry(frame, 'Введите конечное значение', 4)
+        self.step = Entry(frame, 'Введите значение шага', 5)
+        self.show = CheckBox(frame, 'Показать график', 6)
+        self.options = ComboBox(frame, 'Действия с графиком', 7, ["Показать", "Сохранить в файл", "Показать и сохранить", "Игнорировать"])
 
-        start_entry = Entry(frame, )
-        start_entry.grid(row=3, column=2)
-
-        end_label = Label(frame, text="Введите конечное значение")
-        end_label.grid(row=4, column=1)
-
-        end_entry = Entry(frame, )
-        end_entry.grid(row=4, column=2)
-
-        submit = Button(frame, text='Табулировать', )
-        submit.grid(row=5, column=2)
-
+        
+        submit = tk.Button(frame, text='Табулировать', command=self.on_submit_click)
+        submit.grid(row=8, column=2)
         window.mainloop()
 
+    def get_values(self):
+        return {
+        'start': float(self.start.variable.get()),
+        'end': float(self.end.variable.get()),
+        'step': float(self.step.variable.get()),
+        'graph': self.show.variable.get(),
+        'options': self.options.variable.get()
+        }
 
-Form()
+
+
+    def is_fields_valid(self):
+        for field in [self.start, self.end, self.step]:
+            if not field.valid:
+                return False
+        return True
+
+
+    def on_submit_click(self):
+        if self.is_fields_valid():
+            current_values = self.get_values()
+            self.submit_callback(current_values)
+        else:
+            messagebox.showerror(title='Ошибка!', message='Введены недопустимые значения!')
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print([i for i in range(10)])
+from components.Form import Form
+from utils.tabulate import getDataToPrint
+from utils.tabulate import table_to_str
+from utils.tabulate import show_graph
+
+
+
+
+
+def main(values):
+	start, end, delta = values["start"], values["end"], values["step"]
+	graph, options = values["graph"], values["options"]
+	table, maxlen = getDataToPrint(start, end, delta)
+
+	table_str = table_to_str(table, maxlen)
+	print(table_str)
+
+	show_graph(start, end, delta, table)
+
+
+Form(main)
+
+
+
+
